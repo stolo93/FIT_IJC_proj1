edae9e54cb16aeec6ac6fe317db3f80fdf66ac27
Samuel <xstola03@stud.fit.vutbr.cz>
2022-03-03 22:39:12 +0100

    Started error branch

---
+++ error.c
@@ -0,0 +1,23 @@
+/**
+ * @file error.c
+ * @author Samuel Stolarik (xstola03@stud.fit.vutbr.cz)
+ * @brief implementation of error handling functions from error.h
+ * @version 0.1
+ * @date 2022-03-03
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "headers/error.h"
+
+
+void warning_msg(const char *fmt, ...)
+{
+
+}
+
+void error_exit(const char *fmt, ...)
+{
+    
+}
---
+++ headers/error.h
@@ -0,0 +1,24 @@
+/**
+ * @file error.h
+ * @author Samuel Stolarik (xstola03@fit.vutbr.cz)
+ * @brief error handling header file
+ * @version 0.1
+ * @date 2022-03-01
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+#ifndef _ERROR_H
+#define _ERROR_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
+
+void warning_msg(const char *fmt, ...);
+
+void error_exit(const char *fmt, ...);
+
+#endif //_error_h
