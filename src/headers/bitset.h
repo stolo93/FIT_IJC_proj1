a99b4ffa95a54bcde2ae7ec3ff911385908108a8
Samuel <xstola03@stud.fit.vutbr.cz>
2022-03-07 22:56:18 +0100

    Hopefully fixed bitset.h

--- src/headers/bitset.h
+++ src/headers/bitset.h
@@ -32,9 +32,9 @@ typedef unsigned long int bitset_index_t;
 #define bitset_create(name, size)\
     static_assert(size > 0);\
     unsigned long int _true_size = (size) + 1;\
-    unsigned long int #name[_true_size];\
-    #name[0] = (size);\
-    for (size_t i = 1; i < _true_size; i++) #name[i] = 0;
+    unsigned long int name[_true_size];\
+    name[0] = (size);\
+    for (size_t i = 1; i < _true_size; i++) name[i] = 0;
 
 
 /**
@@ -45,23 +45,23 @@ typedef unsigned long int bitset_index_t;
 #define bitset_alloc(name, size)\
     static_assert(size > 0);\
     unsigned long _true_size = (size) + 1;\
-    bitset_t #name = malloc(sizeof(unsigned long) * _true_size);\
-    #name[0] = (size);\
-    for (size_t i = 1; i < _true_size; i++) #name[i] = 0;
+    bitset_t name = malloc(sizeof(unsigned long) * _true_size);\
+    name[0] = (size);\
+    for (size_t i = 1; i < _true_size; i++) name[i] = 0;
 
 /**
  * @brief free bitset created with bitset_alloc
  * 
  */
 #define bitset_free(name)\
-    free(#name);
+    free(name);
 
 /**
  * @brief return size of the bitset "name"
  * 
  */
 #define bitset_size(name)\
-    #name[0]
+    name[0]
 
 /**
  * @brief sets bit on index "index" to either 1 or 0
@@ -70,18 +70,18 @@ typedef unsigned long int bitset_index_t;
  * first it checks if index is valid (not out of bitset)
  */
 #define bitset_setbit(name, index, expr)\
-    bitset_index_t _index_max = _BITS_IN_LONG * bitset_size(#name);\
+    bitset_index_t _index_max = _BITS_IN_LONG * bitset_size(name);\
     if ((index) > _index_max) error_exit("Index: %lu is higher than max index: (%lu)\n", (unsigned long) (index), (unsigned long) _index_max);\
     bitset_index_t _index_real = (index) / _BITS_IN_LONG + 1;\
     unsigned int _index_offset = _BITS_IN_LONG - (index) % _BITS_IN_LONG;\
-    if (expr) #name[_index_real] |= 1u << (_index_offset - 1);\
-    else #name[_index_real] &= ~(1u << (_index_offset - 1));
+    if (expr) name[_index_real] |= 1u << (_index_offset - 1);\
+    else name[_index_real] &= ~(1u << (_index_offset - 1));
 
 /**
  * @brief returns value bit in "name" on "index"
  * //TODO check boundries of the bitset
  */
 #define bitset_getbit(name, index)\
-    #name[(index)/_BITS_IN_LONG +1] & 1u << _BITS_IN_LONG - (index) % _BITS_IN_LONG
+    name[(index)/_BITS_IN_LONG +1] & 1u << _BITS_IN_LONG - (index) % _BITS_IN_LONG
 
 #endif //_BITSET_H\ No newline at end of file
