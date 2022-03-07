ddc1579a6378b33942d59bdf7a06cdeef7a6b0e1
Samuel <xstola03@stud.fit.vutbr.cz>
2022-03-07 23:59:26 +0100

    Fixed issue with multiple use of macros

--- src/headers/bitset.h
+++ src/headers/bitset.h
@@ -9,7 +9,6 @@
  * 
  */
 
-//TODO make multiple uses of macros in one program possible
 #ifndef _BITSET_H
 #define _BITSET_H
 
@@ -32,10 +31,9 @@ typedef unsigned long int bitset_index_t;
  */
 #define bitset_create(name, size)\
     static_assert(size > 0);\
-    unsigned long int _true_size = (size) + 1;\
-    unsigned long int name[_true_size];\
+    unsigned long int name[(size) + 1];\
     name[0] = (size);\
-    for (size_t i = 1; i < _true_size; i++) name[i] = 0;
+    for (size_t i = 1; i < (size) + 1; i++) name[i] = 0
 
 
 /**
@@ -45,17 +43,16 @@ typedef unsigned long int bitset_index_t;
  */
 #define bitset_alloc(name, size)\
     static_assert(size > 0);\
-    unsigned long _true_size = (size) + 1;\
-    bitset_t name = malloc(sizeof(unsigned long) * _true_size);\
+    bitset_t name = malloc(sizeof(unsigned long) * ((size) + 1));\
     name[0] = (size);\
-    for (size_t i = 1; i < _true_size; i++) name[i] = 0;
+    for (size_t i = 1; i < (size) + 1; i++) name[i] = 0
 
 /**
  * @brief free bitset created with bitset_alloc
  * 
  */
 #define bitset_free(name)\
-    free(name);
+    free(name)
 
 /**
  * @brief return size of the bitset "name"
@@ -70,13 +67,14 @@ typedef unsigned long int bitset_index_t;
  * 
  * first it checks if index is valid (not out of bitset)
  */
-#define bitset_setbit(name, index, expr)\
+#define bitset_setbit(name, index, expr)({\
     bitset_index_t _index_max = _BITS_IN_LONG * bitset_size(name);\
     if ((index) > _index_max) error_exit("Index: %lu is higher than max index: (%lu)\n", (unsigned long) (index), (unsigned long) _index_max);\
     bitset_index_t _index_real = (index) / _BITS_IN_LONG + 1;\
     unsigned int _index_offset = _MAX_LSHIFT - (index) % _BITS_IN_LONG;\
     if (expr) name[_index_real] |= 1ul << _index_offset;\
-    else name[_index_real] &= ~(1ul << _index_offset - 1);
+    else name[_index_real] &= ~(1ul << _index_offset - 1);\
+    })
 
 /**
  * @brief returns value bit in "name" on "index"
