35862682df645a85983f138d1d162f73f2310254
Samuel <xstola03@stud.fit.vutbr.cz>
2022-03-11 22:27:37 +0100

    created ppm.h and ppm.c

---
+++ ppm.c
@@ -0,0 +1,10 @@
+/**
+ * @file ppm.c
+ * @author Samuel Stolarik (xstola03@stud.fit.vutbr.cz), FIT
+ * 
+ * @brief IJC-DU1, part B
+ * implementation of functions defined in ppm.h header file
+ * Used compiler: gcc (GCC) 11.2.0
+ * 
+ * @date 2022-03-03
+ */
---
+++ ppm.h
@@ -0,0 +1,9 @@
+/**
+ * @file ppm.h
+ * @author Samuel Stolarik (xstola03@stud.fit.vutbr.cz), FIT
+ * 
+ * @brief IJC-DU1, part B
+ * header file defining data structures and functions for operating on .ppm files
+ * 
+ * @date 2022-03-03
+ */
