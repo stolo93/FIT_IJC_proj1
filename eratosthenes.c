9f832ecdf8bc47bf128d1cc8583945b00dc3bd45
Samuel <xstola03@stud.fit.vutbr.cz>
2022-03-23 10:04:14 +0100

    deleted unused eratosthenes functions

--- eratosthenes.c
+++ eratosthenes.c
@@ -16,21 +16,6 @@
 
 #include "bitset.h"
 
-// void Eratosthenes(bitset_t array)
-// {
-//     bitset_setbit(array, 0, 1); 
-//     bitset_setbit(array, 1, 1);
-
-//     for (bitset_index_t i = 2; i <= sqrt(bitset_size(array)); i++)
-//     {   
-//         for (bitset_index_t j = i*i; j <= bitset_size(array) && !bitset_getbit(array, i); j+=i)
-//         {
-//             bitset_setbit(array, j, 1);
-//         }
-//     }
-//     return;
-// }
-
 void Eratosthenes(bitset_t array)
 {
     
